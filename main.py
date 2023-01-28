f930d8a386080261c59920ca24e32af2c5b94220
donghhan <apensia914@gmail.com>
2023-01-28 13:34:57 +0900

    Home section finished

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 
 @app.route("/")
 def home():
-    return "This is home page"
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
