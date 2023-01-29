fec6937784937a3223961c58b2f1d59e76407022
donghhan <apensia914@gmail.com>
2023-01-30 07:58:48 +0900

    Pagination count added

--- parser/jobsdb.py
+++ parser/jobsdb.py
@@ -5,55 +5,67 @@ from bs4 import BeautifulSoup
 class JobsDBParser:
     base_url = "https://th.jobsdb.com/th/search-jobs"
 
-    def __init__(self, keyword, page):
+    def __init__(self, keyword):
         self.keyword: str = keyword
-        self.page: int = page
 
-    def parser(self):
-        url = f"{self.base_url}/{self.keyword}/{self.page}"
-        r = requests.get(url, auth=("user", "pass"))
+    def page_counts(self):
+        url = f"{self.base_url}/{self.keyword}"
+        r = requests.get(url)
 
         if r.status_code != 200:
-            raise ValueError("Cannot parse.")
+            raise ValueError("Internal server error. Try later.")
 
         soup = BeautifulSoup(r.text, "html.parser")
-        jobs = soup.select('div[data-automation="jobListing"]')
+        pagination = soup.select_one("select#pagination")
+        total_pages = int(pagination.find_all("option", recursive=False)[-1].text)
+        return total_pages
 
+    def parser(self):
         results = []
 
-        for job_lists in jobs:
-            job_posts = job_lists.select("article")
+        for i in range(self.page_counts()):
+            url = f"{self.base_url}/{self.keyword}/{i + 1}"
+            r = requests.get(url)
 
-            for job_post in job_posts:
-                link = job_post.find(attrs={"target": "_top"})
-                job_url = f'https://th.jobsdb.com{link.get("href")}'
-                job_title = link.find("span").text
-                company_name = job_post.find(
-                    attrs={"data-automation": "jobCardCompanyLink"}
-                ).text
-                location = job_post.find(
-                    attrs={"data-automation": "jobCardLocationLink"}
-                ).text
-                company_logo_src = job_post.find(
-                    "img", attrs={"class": "sx2jih0 rqoqz6"}
-                )
-                if company_logo_src:
-                    company_logo = company_logo_src.get("src")
-                else:
-                    company_logo = "N/A"
+            if r.status_code != 200:
+                raise ValueError("Internal server error. Try later.")
 
-                data = {
-                    "job_title": job_title,
-                    "job_url": job_url,
-                    "company_name": company_name,
-                    "location": location,
-                    "company_logo": company_logo,
-                }
+            soup = BeautifulSoup(r.text, "html.parser")
+            jobs = soup.select('div[data-automation="jobListing"]')
 
-                results.append(data)
+            for job_lists in jobs:
+                job_posts = job_lists.select("article")
+
+                for job_post in job_posts:
+                    link = job_post.find(attrs={"target": "_top"})
+                    job_url = f'https://th.jobsdb.com{link.get("href")}'
+                    job_title = link.find("span").text
+                    company_name = job_post.find(
+                        attrs={"data-automation": "jobCardCompanyLink"}
+                    ).text
+                    location = job_post.find(
+                        attrs={"data-automation": "jobCardLocationLink"}
+                    ).text
+                    company_logo_src = job_post.find(
+                        "img", attrs={"class": "sx2jih0 rqoqz6"}
+                    )
+                    if company_logo_src:
+                        company_logo = company_logo_src.get("src")
+                    else:
+                        company_logo = "N/A"
+
+                    data = {
+                        "job_title": job_title,
+                        "job_url": job_url,
+                        "company_name": company_name,
+                        "location": location,
+                        "company_logo": company_logo,
+                    }
+
+                    results.append(data)
 
         return results
 
 
-a = JobsDBParser("frontend", 1).parser()
+a = len(JobsDBParser("frontend").parser())
 print(a)
